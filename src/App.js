e5b2f2e724544e5d6fb4c5e2da28e654484b9732
BadTripp <renato.turetta00@gmail.com>
2021-09-27 15:50:56 +0200

    add Header components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Layout/Header';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header></Header>
+    </React.Fragment>
+  );
+}
+
+export default App;
